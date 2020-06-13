feae091e2f0bb924768ec19d7a5a32286bc34040
tanmaysharma786 <64467958+tanmaysharma786@users.noreply.github.com>
2020-06-13 17:16:10 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<body bgcolor='black'>
+Welcome to my page.
+These are my friends Shubh(extreme left), Yashika(mid) and Shivansh(extreme right).
+<pre>
+<?php
+
+echo "<img src='https://.cloudfront.net/Tanmay's_friend.jpg' width='600' height='300'
+
+?>
+</pre>
