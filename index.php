3dab0a5ad91562a8bac635d1d4b677c97d41a9c6
tanmaysharma786 <64467958+tanmaysharma786@users.noreply.github.com>
2020-06-15 00:48:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ These are my friends Shubh Gupta(extreme left), Yashika Khandelwal(mid) and Shiv
 <html>
 <body>
 <?php
-echo "<img src='https://tanmay786.s3.ap-south-1.amazonaws.com/Tanmay's_friends.jpeg' , width='600', height='300' />";
+echo "<img src='https://d1vg6nrps5v984.cloudfront.net
+/Tanmay's_friends.jpeg' , width='600', height='300' />";
 ?>
 </body>
 </html>
