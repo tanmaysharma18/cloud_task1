af9bef8f4381d0fa360d36167d6f70cce52d9481
tanmaysharma786 <64467958+tanmaysharma786@users.noreply.github.com>
2020-06-15 19:22:33 +0530

    final

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ These are my friends Shubh (Extreme Left), Yashika (mid) and Shivansh (Extreme R
 <body>
 <?php
 echo "<img src='https://d1vg6nrps5v984.cloudfront.net
-/Tanmay's_friends.jpeg' width='499' height='392' />";
+/Tanmay's_friends.jpg' width='499' height='392' />";
 ?>
 </body>
 </html>
