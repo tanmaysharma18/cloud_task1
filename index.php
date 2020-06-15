cfbbfa37bd33a5d53d4dfdfb2d8dac93810fc2f0
tanmaysharma786 <64467958+tanmaysharma786@users.noreply.github.com>
2020-06-15 18:28:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 Welcome to Tanmay Sharma's page.
-These are my friends Shubh Gupta(extreme left), Yashika Khandelwal(mid) and Shivansh Khandelwal(extreme right).
+These are my friends Shubh (Extreme Left), Yashika (mid) and Shivansh (Extreme Right).
 <pre>
 <html>
 <body>
 <?php
 echo "<img src='https://d1vg6nrps5v984.cloudfront.net
-/Tanmay's_friends.jpeg' width='300' height='140' />";
+/Tanmay's_friends.jpeg' width='300' height='300' />";
 ?>
 </body>
 </html>
